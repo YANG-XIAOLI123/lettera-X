66eca6c5a673d238ab66a0cffd4f3ff2e73e73ee
YANG-XIAOLI123 <yxl980816@gmail.com>
2025-04-01 11:27:38 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 export const configurazione = {
-  testo: "G",
+  testo: "X",
 
   dimensione: 0.8,
   interlinea: 0.7,
@@ -9,7 +9,7 @@ export const configurazione = {
   sensibilitàMicrofonoBase: 1,
   densitàPuntiBase: 1,
 
-  nascondiInterfaccia: false,
+  nascondiInterfaccia: true,
 };
 
 /**
@@ -42,18 +42,10 @@ export function disegnaPunto({
   beta = 0,
   gamma = 0,
 }) {
-  const size = sin((frameCount + indice) * 6) * ((volume * unita) / 2) * unita;
-
-  if (indice % 2 == 0) {
-    fill("black");
-  } else {
-    fill("white");
-  }
-  noStroke();
-
   push();
   translate(x, y);
-  ellipse(0, 0, size);
+  rotate(frameCount);
+  rect(0, 0, 40);
   pop();
 }
 
